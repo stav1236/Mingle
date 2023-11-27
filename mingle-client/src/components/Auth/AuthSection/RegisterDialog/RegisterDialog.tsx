c47768f42abc96fd4586acdc4c89551b86d1adf6
stav1236 <stavmaor1@gmail.com>
2023-11-27 11:19:26 +0200

    responsive dialog

--- mingle-client/src/components/Auth/AuthSection/RegisterDialog/RegisterDialog.tsx
+++ mingle-client/src/components/Auth/AuthSection/RegisterDialog/RegisterDialog.tsx
@@ -7,10 +7,16 @@ import {
   DialogProps,
   DialogTitle,
   FormControl,
+  FormControlLabel,
+  FormLabel,
+  InputAdornment,
   InputLabel,
   MenuItem,
+  Radio,
+  RadioGroup,
   Select,
   TextField,
+  Typography,
 } from "@mui/material";
 import { useState } from "react";
 
@@ -46,72 +52,127 @@ const RegisterDialog = (props: RegisterDialogProps) => {
       <Box
         sx={{
           maxWidth: "90vw",
-          minWidth: "30vw",
-          width: 300,
+          minWidth: "25vw",
           maxHeight: "90vh",
           minHeight: "50vh",
           height: 500,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
         }}
       >
-        <DialogTitle>Register</DialogTitle>
-        <DialogContent>
+        <Typography sx={{ pt: 2 }} variant="h5" fontWeight="bold">
+          הרשמה
+        </Typography>
+        <Box
+          sx={{
+            p: "4%",
+            height: "76%",
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+        >
+          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+            <TextField
+              sx={{
+                width: "48%",
+                "& input": {
+                  height: 15,
+                },
+              }}
+              label="שם פרטי"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+            <TextField
+              sx={{
+                width: "48%",
+                "& input": {
+                  height: 15,
+                },
+              }}
+              label="שם משפחה"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </Box>
           <TextField
-            label="First Name"
-            fullWidth
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-          <TextField
-            label="Last Name"
-            fullWidth
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-          />
-          <TextField
-            label="Phone Number/Email"
+            sx={{
+              "& input": {
+                height: 15,
+              },
+            }}
+            label="אמייל / טלפון"
             fullWidth
             value={phoneNumberEmail}
             onChange={(e) => setPhoneNumberEmail(e.target.value)}
           />
           <TextField
-            label="Password"
+            sx={{
+              "& input": {
+                height: 15,
+              },
+            }}
+            label="סיסמא"
             type="password"
             fullWidth
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
           <TextField
-            label="Confirm Password"
+            sx={{
+              "& input": {
+                height: 15,
+              },
+            }}
+            label="אימות סיסמא"
             type="password"
             fullWidth
             value={passwordValidate}
             onChange={(e) => setPasswordValidate(e.target.value)}
           />
           <TextField
-            label="Birthdate"
-            type="date"
+            sx={{
+              "& input": {
+                height: 15,
+              },
+            }}
+            InputProps={{
+              inputProps: { style: { textAlign: "left" } },
+            }}
             fullWidth
+            type="date"
+            label="תאריך לידה"
             value={birthdate}
             onChange={(e) => setBirthdate(e.target.value)}
           />
           <FormControl fullWidth>
-            <InputLabel>Gender</InputLabel>
-            <Select
-              value={gender}
-              onChange={(e) => setGender(e.target.value as string)}
+            <FormLabel id="demo-row-radio-buttons-group-label">
+              Gender
+            </FormLabel>
+            <RadioGroup
+              row
+              aria-labelledby="demo-row-radio-buttons-group-label"
+              name="row-radio-buttons-group"
             >
-              <MenuItem value="male">Male</MenuItem>
-              <MenuItem value="female">Female</MenuItem>
-              <MenuItem value="other">Other</MenuItem>
-            </Select>
+              <FormControlLabel value="זכר" control={<Radio />} label="זכר" />
+              <FormControlLabel value="נקבה" control={<Radio />} label="נקבה" />
+              <FormControlLabel value="אחר" control={<Radio />} label="אחר" />
+            </RadioGroup>
           </FormControl>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={props.onClose}>Cancel</Button>
-          <Button onClick={handleRegister} color="primary">
-            Register
+        </Box>
+        <Box sx={{ p: 1, width: "100%", display: "flex" }}>
+          <Button
+            sx={{ m: "auto" }}
+            variant="contained"
+            onClick={handleRegister}
+            color="primary"
+          >
+            הרשמה
           </Button>
-        </DialogActions>
+        </Box>
       </Box>
     </Dialog>
   );
