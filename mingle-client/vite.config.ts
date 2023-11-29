1b4b46c5cd7df7bf8db496bee438499084afdb32
unknown <stavmaor1@gmail.com>
2023-11-29 20:44:19 +0200

    alias

--- mingle-client/vite.config.ts
+++ mingle-client/vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+      },
+      {
+        find: "contexts",
+        replacement: fileURLToPath(new URL("./src/contexts", import.meta.url)),
+      },
+      {
+        find: "components",
+        replacement: fileURLToPath(
+          new URL("./src/components", import.meta.url)
+        ),
+      },
+    ],
+  },
+});
